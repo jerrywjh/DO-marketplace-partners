0e7fe39b1e0a12665b13bbc7feedc118fb77964f
Jerry W <jerrywjh@gmail.com>
2022-05-26 19:01:17 +0800

    Update 90-cleanup.sh

--- scripts/90-cleanup.sh
+++ scripts/90-cleanup.sh
@@ -37,13 +37,13 @@ touch /etc/ssh/revoked_keys
 chmod 600 /etc/ssh/revoked_keys
 
 # Securely erase the unused portion of the filesystem
-GREEN='\033[0;32m'
-NC='\033[0m'
-printf "\n${GREEN}Writing zeros to the remaining disk space to securely
-erase the unused portion of the file system.
-Depending on your disk size this may take several minutes.
-The secure erase will complete successfully when you see:${NC}
-    dd: writing to '/zerofile': No space left on device\n
-Beginning secure erase now\n"
-
-dd if=/dev/zero of=/zerofile bs=4096 || rm /zerofile
+# GREEN='\033[0;32m'
+# NC='\033[0m'
+# printf "\n${GREEN}Writing zeros to the remaining disk space to securely
+# erase the unused portion of the file system.
+# Depending on your disk size this may take several minutes.
+# The secure erase will complete successfully when you see:${NC}
+#    dd: writing to '/zerofile': No space left on device\n
+# Beginning secure erase now\n"
+#
+# dd if=/dev/zero of=/zerofile bs=4096 || rm /zerofile
