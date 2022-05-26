2f42ac668951a6c1f3cf44feea016aab6989e429
Jerry W <jerrywjh@gmail.com>
2022-05-26 19:35:29 +0800

    Update 90-cleanup.sh

--- scripts/90-cleanup.sh
+++ scripts/90-cleanup.sh
@@ -14,16 +14,16 @@ if [[ ! -d /tmp ]]; then
 fi
 chmod 1777 /tmp
 
-# if [ -n "$(command -v yum)" ]; then
-#  yum update -y
-#  yum clean all
-# elif [ -n "$(command -v apt-get)" ]; then
-#  export DEBIAN_FRONTEND=noninteractive
-#  apt-get -y update
-#  apt-get -o Dpkg::Options::="--force-confold" upgrade -q -y --force-yes
-#  apt-get -y autoremove
-#  apt-get -y autoclean
-# fi
+if [ -n "$(command -v yum)" ]; then
+  yum update -y
+  yum clean all
+elif [ -n "$(command -v apt-get)" ]; then
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get -y update
+  apt-get -o Dpkg::Options::="--force-confold" upgrade -q -y --force-yes
+  apt-get -y autoremove
+  apt-get -y autoclean
+fi
 
 rm -rf /tmp/* /var/tmp/*
 history -c
@@ -37,13 +37,13 @@ touch /etc/ssh/revoked_keys
 chmod 600 /etc/ssh/revoked_keys
 
 # Securely erase the unused portion of the filesystem
-# GREEN='\033[0;32m'
-# NC='\033[0m'
-# printf "\n${GREEN}Writing zeros to the remaining disk space to securely
-# erase the unused portion of the file system.
-# Depending on your disk size this may take several minutes.
-# The secure erase will complete successfully when you see:${NC}
-#    dd: writing to '/zerofile': No space left on device\n
-# Beginning secure erase now\n"
-#
-# dd if=/dev/zero of=/zerofile bs=4096 || rm /zerofile
+GREEN='\033[0;32m'
+NC='\033[0m'
+printf "\n${GREEN}Writing zeros to the remaining disk space to securely
+ erase the unused portion of the file system.
+ Depending on your disk size this may take several minutes.
+ The secure erase will complete successfully when you see:${NC}
+    dd: writing to '/zerofile': No space left on device\n
+ Beginning secure erase now\n"
+
+dd if=/dev/zero of=/zerofile bs=4096 || rm /zerofile
